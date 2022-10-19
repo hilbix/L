a200175de37b25491d37c9cc16f7e3d540fb49b9
Valentin Hilbig <webmaster@scylla-charybdis.com>
2022-10-19 03:02:16 +0200

    L.h modified

--- L.h
+++ L.h
@@ -1,25 +1,8 @@
-/*
- * This is free software as in free beer, free speech and free baby.
- *
- * GCCFLAGS:	-Wno-unused-function
+/* This Works is placed under the terms of the Copyright Less License,
+ * see file COPYRIGHT.CLL.  USE AT OWN RISK, ABSOLUTELY NO WARRANTY.
+ * Read: This is free software as in free beer, free speech and free baby.
  *
  * vim:	ts=8
- *
- * I am a bit curious about
- * who the first idiot was to think that it perhaps is a
- * good idea to redefine TABs to some other default than 8:
- *	https://www.imdb.com/title/tt0387808/
- * And here a reference to the proper default, the only one I accept:
- *	https://www.ibm.com/docs/de/aix/7.1?topic=t-tabs-command
- *	https://en.wikipedia.org/wiki/Tab_key#Tab_characters
- * Please do not mix TAB with indentation.
- * I use indentation of 2 for over 35 years now
- * with braces which belong together being either on the same row or column
- * (else I am unable to spot which braces belong together,
- *  sorry, this is some of my human limitations I cannot overcome).
- * And I expect TAB to stay at column 8 for much longer!
- *
- * egrep -w 'FORMAT|Loops' l.c | fgrep -v ', NULL);'
  */
 
 #include <stdio.h>
